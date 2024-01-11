67af21f26b08d14f447f3d569ed5d192abe21cdf
yuukimaru130516 <yuuki130516@gmail.com]>
2024-01-11 15:28:55 +0900

    追加開発

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,6 +1,6 @@
 //scroll_effect
 $(window).scroll(function () {
-    var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+	var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
     var scrollAnimationFunc = function () {
       for (var i = 0; i < scrollAnimationElm.length; i++) {
         var triggerMargin = 100;
